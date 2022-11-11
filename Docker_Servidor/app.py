2b656cf1cb4303591fe9391d6d709c9802bcb556
Guillem Companys Garcia <g.garcia.companys@gmail.com>
2022-11-12 00:44:41 +0100

    12/11

--- Docker_Servidor/app.py
+++ Docker_Servidor/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+ return render_template('index.html')
 
 @app.route('/config', methods=['GET', 'POST'])
 def config():
